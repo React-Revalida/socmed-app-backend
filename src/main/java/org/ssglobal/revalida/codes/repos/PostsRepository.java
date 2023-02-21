a9604f7ddad61f87317add358d5e7036fafc99bf
LPAULINO@win-wks301474 <paulinolawrence08@gmail.com>
2023-02-21 17:25:14 +0800

    Comments to the error code in Posts Repo

--- src/main/java/org/ssglobal/revalida/codes/repos/PostsRepository.java
+++ src/main/java/org/ssglobal/revalida/codes/repos/PostsRepository.java
@@ -1,16 +1,15 @@
 package org.ssglobal.revalida.codes.repos;
 
-import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.ssglobal.revalida.codes.model.Posts;
 
 public interface PostsRepository extends JpaRepository<Posts, Integer> {
 	
-	@Query("select p.postId, p.message, p.imageUrl, p.timestamp, p.deleted, p.user from Posts p where p.user.username = :username")
-	Set<Posts> findAllPostsByUsername(@Param("username") String username);
+//  Bug: ConverterNotFoundException 
+//	Workaround: Used findAll method of JpaRepo to fetch data
+	
+//	@Query("select p.postId, p.message, p.imageUrl, p.timestamp, p.deleted, p.user from Posts p where p.user.username = :username")
+//	Set<Posts> findAllPostsByUsername(@Param("username") String username);
 		
 	
 }
