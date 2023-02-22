797ea9073fff479f1a276524ce0198dde993514a
brynpryl77 <brynpryl77@gmail.com>
2023-02-22 16:19:21 +0800

    changed controller on user update

--- src/main/java/org/ssglobal/revalida/codes/controller/AppUserController.java
+++ src/main/java/org/ssglobal/revalida/codes/controller/AppUserController.java
@@ -11,14 +11,18 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.ssglobal.revalida.codes.dto.AddressDTO;
 import org.ssglobal.revalida.codes.dto.AppUserDTO;
+import org.ssglobal.revalida.codes.dto.ProfileDTO;
 import org.ssglobal.revalida.codes.dto.RegistrationDTO;
 import org.ssglobal.revalida.codes.service.AppUserService;
+import org.ssglobal.revalida.codes.service.ProfileService;
 
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
@@ -32,8 +36,11 @@ public class AppUserController {
 
     private final AppUserService appUserService;
 
-    public AppUserController(AppUserService appUserService) {
+    private final ProfileService profileService;
+
+    public AppUserController(AppUserService appUserService, ProfileService profileService) {
         this.appUserService = appUserService;
+        this.profileService = profileService;
     }
 
     @PostMapping
@@ -51,4 +58,28 @@ public class AppUserController {
         AppUserDTO appUserDTO = appUserService.findByUsername(user);
         return new ResponseEntity<>(appUserDTO, HttpStatus.OK);
     }
+
+    @PostMapping("/me/update-profile")
+    public ResponseEntity<Boolean> updateMyProfile(
+            @RequestHeader(name = HttpHeaders.AUTHORIZATION) String token,
+            @RequestPart("user") @Valid final ProfileDTO profileDTO,
+            @RequestPart("profile") MultipartFile profilePic) throws IOException {
+        String jwtToken = token.replace("Bearer ", "");
+        Jwt jwt = jwtDecoder.decode(jwtToken);
+        String username = jwt.getClaim("user");
+        Boolean isUpdated = profileService.updateProfile(username, profileDTO, profilePic);
+        return new ResponseEntity<>(isUpdated, HttpStatus.OK);
+    }
+
+    @PostMapping("/me/update-address")
+    public ResponseEntity<Boolean> updateMyAddress(
+            @RequestHeader(name = HttpHeaders.AUTHORIZATION) String token,
+            @RequestBody @Valid final AddressDTO addressDTO) {
+        String jwtToken = token.replace("Bearer ", "");
+        Jwt jwt = jwtDecoder.decode(jwtToken);
+        String username = jwt.getClaim("user");
+        Boolean isUpdated = profileService.updateOrAddAddress(username, addressDTO);
+        return new ResponseEntity<>(isUpdated, HttpStatus.OK);
+    }
+
 }
