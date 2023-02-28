5b13217adb072d2dca5c5699c9e0c431851b69ee
AynnnUsonn <201811226@gordoncollege.edu.ph>
2023-02-28 15:00:21 +0800

    LikedPost by user

--- src/main/java/org/ssglobal/revalida/codes/repos/LikesRepository.java
+++ src/main/java/org/ssglobal/revalida/codes/repos/LikesRepository.java
@@ -9,11 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.ssglobal.revalida.codes.model.AppUser;
 import org.ssglobal.revalida.codes.model.Likes;
+import org.ssglobal.revalida.codes.model.Posts;
 
 public interface LikesRepository extends JpaRepository<Likes, Integer> {
 
     @Query("select l.user from Likes l where l.post.postId = :post AND l.liked=true") 
 	Set<AppUser> findAllByPostId(@Param("post") Integer post);
+    
+    @Query("select l.post from Likes l where l.user.userId = :user AND l.liked=true") 
+    Set<Posts> findAllLikedPostByUserId(@Param("user") Integer user);
     
     @Modifying
     @Query("delete from Likes l where l.post.postId = :post AND l.user.userId= :user") 
